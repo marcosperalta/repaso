34f6a5dd917832bdab2d963be4946dcc75ad4365
Marcos Peralta <marcosperalta02@gmail.com>
2024-05-17 11:39:03 -0300

    ej0

--- c/lab03/ej0/main.c
+++ c/lab03/ej0/main.c
@@ -12,19 +12,57 @@ static void dump(char a[], unsigned int length) {
     printf("\n\n");
 }
 
-int main(/* -- completar-- */) {
-    FILE *file;
+char *parse_filepath(int argc, char *argv[]) {
+    /* Parse the filepath given by command line argument. */
+    char *result = NULL; // inicializa un puntero tipo 'char *'
+
+    if (argc < 2) { // chequea que la cantidad de argumentos sea 2 o más
+        //print_help(argv[0]);
+        printf("mal llamado el archivo\n");
+        exit(EXIT_FAILURE);
+    }
+
+    result = argv[1]; // asigna el argumento de esa posición a result para retornarla
+
+    return result; // retorna el filepath que esta en argv[1], que se dió como argumento a la hora de iniciar la aplicación
+}
+
+unsigned int data_from_file(const char *path,
+unsigned int indexes[], char letters[], unsigned int max_size) {
+    unsigned int length = 0;
+    FILE *pfile = fopen(path, "r");
+    if (pfile == NULL) {
+        printf("No se pudo abrir el archivo.\n");
+        exit(EXIT_FAILURE);
+    }
+    length = max_size;
+    printf("Se pudo abrir el archivo!\n");
+    unsigned int i = 0;
+    while (!feof(pfile)) {
+        fscanf(pfile, "%u %c", &indexes[i], &letters[i]);
+        i++;
+    }
+    fclose(pfile);
+    printf("\nlength: %u", length);
+    length = i;
+    return length;
+}
+
+int main(int argc, char *argv[]) {
+    //FILE *file;
+    
+    char *path = parse_filepath(argc, argv);
+    
     unsigned int indexes[MAX_SIZE];
     char letters[MAX_SIZE];
-    char sorted[MAX_SIZE];
+    //char sorted[MAX_SIZE];
     unsigned int length=0; 
-    //  .----------^
-    //  :
-    // Debe guardarse aqui la cantidad de elementos leidos del archivo
-    
-    /* -- completar -- */
-
-    dump(sorted, length);
+   
+    length = data_from_file(path, indexes, letters, MAX_SIZE);
+    printf("\nlength: %u\n", length);
+    //dump((char*)indexes, length);
+    dump(letters, length);
+    //dump(sorted, length);
 
     return EXIT_SUCCESS;
 }
